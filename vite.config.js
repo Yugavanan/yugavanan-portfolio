6fa7adc0db410bc5afb6796591bd255959298e7f
YUGAVANAN <150040880+Yugavanan@users.noreply.github.com>
2025-11-04 15:50:45 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./Yuga-Portfolio",
+  base: '/yugavanan-portfolio/',
   build: {
     outDir: 'dist',
   },
-});
+})
